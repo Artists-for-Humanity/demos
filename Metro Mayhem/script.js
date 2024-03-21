0ec8759e9ea6ff40407e05da3d859a2d991127b5
Vinh <145184639+v-iinh@users.noreply.github.com>
2024-03-21 16:06:02 -0400

    updated placeholders

--- Metro Mayhem/script.js
+++ Metro Mayhem/script.js
@@ -50,14 +50,14 @@ function update(){
   if(buttonCounter == 5){
     var body = document.getElementsByTagName('body')[0];
     body.style.backgroundImage = 'url(assets/2.0.png)';
-    tool.style.backgroundImage = 'url(assets/scissors.png)'
+    tool.style.backgroundImage = 'url(assets/broom.png)'
     text.innerHTML = "Dang it, looks like we have ALOT more work to do."
     transition()
     rotateToolOnce()
   }
   if(buttonCounter == 6){
     var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(assets/2.0.png)';
+    body.style.backgroundImage = 'url(assets/2.1.png)';
     tool.style.backgroundImage = 'url(assets/paintbrush.png)'
     text.innerHTML = "Well don't just stand there, there's more work to be done."
     transition()
@@ -65,7 +65,7 @@ function update(){
   }
   if(buttonCounter == 7){
     var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(assets/2.0.png)';
+    body.style.backgroundImage = 'url(assets/2.2.png)';
     tool.style.backgroundImage = 'url(assets/hammer.png)'
     text.innerHTML = "Just trust me. The hard work will pay off, I can feel it."
     transition()
@@ -73,7 +73,7 @@ function update(){
   }
   if(buttonCounter == 8){
     var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(assets/2.0.png)';
+    body.style.backgroundImage = 'url(assets/2.3.png)';
     tool.style.backgroundImage = 'url(assets/scissors.png)'
     text.innerHTML = "Good work, I like where this is going. Almost there...!"
     transition()
@@ -81,7 +81,7 @@ function update(){
   }
   if(buttonCounter == 9){
     var body = document.getElementsByTagName('body')[0];
-    body.style.backgroundImage = 'url(assets/2.0.png)';
+    body.style.backgroundImage = 'url(assets/2.4.png)';
     tool.style.backgroundImage = 'url(assets/door.png)'
     text.innerHTML = "With YOUR help, this is what our Red Line can be like."
     transition()
@@ -90,14 +90,14 @@ function update(){
   if(buttonCounter >= 10){
     if(buttonCounter%2 == 0){
       var body = document.getElementsByTagName('body')[0];
-      body.style.backgroundImage = 'url(assets/1.0.png)';
+      body.style.backgroundImage = 'url(assets/3.0.png)';
       tool.style.backgroundImage = 'url(assets/view.png)'
       text.innerHTML = ""
       transition()
       rotateToolOnce()
     } else {
       var body = document.getElementsByTagName('body')[0];
-      body.style.backgroundImage = 'url(assets/2.0.png)';
+      body.style.backgroundImage = 'url(assets/3.1.png)';
       tool.style.backgroundImage = 'url(assets/view.png)'
       text.innerHTML = ""
       transition()
--- README.md
+++
@@ -1,1 +0,0 @@
-# flow-free-clone
