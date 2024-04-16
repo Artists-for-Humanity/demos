4960793ea3bca76dfbabacf9e88a716d43bc18d0
v-iinh <officiallyvt@gmail.com>
2024-04-16 13:49:30 -0400

    updated popup

--- World's Legacy/scripts/popup.js
+++ World's Legacy/scripts/popup.js
@@ -1,22 +1,41 @@
-const text = document.getElementById("text");
-document.addEventListener("click", function(event) {
-    if (event.target.id === "United_States_1946_2016") {
-        text.innerHTML = "NAME: America" + 
-        "<br>POPULATION: 300,000,000" +
-        "<br>GDP: 21 Trillion";
-    }
+let currentYear = 2020;
+function updateCurrentYear() {
+    currentYear = parseInt($('.range input').val());
+    data.style.display = "none";
+}
+$(function(){
+    updateCurrentYear();
+    $('.range input').on('mousemove', updateCurrentYear);
 });
+
+const data = document.getElementById("data");
+const text = document.createElement("div");
+text.id = "text";
+data.appendChild(text);
 
 document.addEventListener("click", function(event) {
     if (event.target.tagName === "path") {
         const fillColor = event.target.getAttribute("fill");
-        text.innerHTML = "Fill Color: " + fillColor;
-    }
-});
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        
+        if (fillColor === "#7f0000" && currentYear === 1940) {
+            data.style.display = "flex";
+            text.innerHTML = 
+            "NAME: China" + 
+            "<br>POPULATION: 1,439,323,776" +
+            "<br>GDP: $14.3 trillion";
+        } else if (fillColor === "#081d58") {
+            data.style.display = "flex";
+            text.innerHTML = 
+            "NAME: America" + 
+            "<br>POPULATION: 331,002,651" +
+            "<br>GDP: $21.43 trillion";
+        } else {
+            data.style.display = "none";
+        }
 
-document.addEventListener("click", function(event) {
-    if (event.target.tagName === "path") {
-        const fillColor = event.target.getAttribute("fill");
-        text.innerHTML = "Fill Color: " + fillColor;
+        data.style.top = mouseY + "px";
+        data.style.left = mouseX + "px";
     }
 });
